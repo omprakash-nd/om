f517a3a2b4688d12f1761cabf689c0f63c09084c
omprakash-nd <32511552+omprakash-nd@users.noreply.github.com>
2017-10-20 18:39:29 +0530

    employee account management

---
+++ datastore.py
@@ -0,0 +1,22 @@
+def empacc():
+    print "New Account Creation \n"
+    
+    print "Enter Initial:"
+    eni = str(raw_input())
+    print "Enter firstname:"
+    fn = str(raw_input())
+    print "Enter Lastname:"
+    ln = str(raw_input())
+    name = eni + fn + ln
+
+    print "Enter DOB"
+    dob = str(raw_input())
+
+    print "Enter Blood Group:"
+    bld = str(raw_input())
+    return name,dob,bld    
+
+
+    
+
+    
