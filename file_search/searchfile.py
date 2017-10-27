b4fefb6a908a9b7e8ec3925abdd252ed28b56310
omprakashnd <omprakashnd94@gmail.com>
2017-10-27 19:24:20 +0530

    File Search Operation

--- file_search/searchfile.py
+++ file_search/searchfile.py
@@ -2,41 +2,43 @@ import os
 
 file_name = str(raw_input("Enter File Name:"))
 
-cur_dir = os.getcwd() # Dir from where search starts can be replaced with any path
+cur_dir = os.getcwd()
 
 while True:
-
     file_list = os.listdir(cur_dir)
     parent_dir = os.path.dirname(cur_dir)
 
     if file_name in file_list:
         print "File Exists in: ", cur_dir
-
+        filename = file_name
         print "Operations"
-        
-        print """1.Write
+        print """0.Search File
+1.Write
 2.Read
 3.Append
 4.create a new file
-Other keys for Exit"""
-
+5.Search Word
+Other keys for Exit \n """
         check = int(raw_input("Enter Your Choice"))
-
+        
         if check == 1:
-            
+            file_name = filename
             i = str(raw_input("Enter your data:")) 
             data = open(file_name,'w')
             data.write(i)
             print "Your data Saved in %r" % file_name
-
+        
         elif check == 2:
+            file_name = filename
             data = open(file_name,'r')
             for line in data:
                 print line
+                sample = line.split(' ') 
+                
             data.close()
            
-
         elif check == 3:
+            file_name = filename
             i = str(raw_input("Enter your data:")) 
             data = open(file_name,'a')
             data.write(i)
@@ -44,19 +46,27 @@ Other keys for Exit"""
             for line in data:
                 print line
             data.close()
-
+        
         elif check == 4:
             name = str(raw_input("Enter File name:"))
             data = open(name.strip()+".txt","w")
             print "file created.."
- 
+        
+        elif check == 5:
+            file_name = filename
+            name = str(raw_input("Enter the word you search in file.."))
+            for word in sample:
+                if word == name: 
+                    print "True"
+                else:
+                    print "false"
+               
         else:
             print """Your Pressed Wroung Key..!!
-                    Bye..Bye.."""
+Bye..Bye.."""
             exit()
-    
-    else:
 
+    else:
         if cur_dir == parent_dir: #if dir is root dir
             print "File not found"
             break
