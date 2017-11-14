2e483f96e5be23a0b55eb72a1fbd2705e76ffc76
omprakashnd <omprakashnd94@gmail.com>
2017-11-14 19:12:05 +0530

    Html Tag And Attribute Detection

--- Python_Exercises/Problem1.py
+++ Python_Exercises/Problem1.py
@@ -3,94 +3,125 @@ import os,re
 
 class CheckHTML():
 
-    def get_FileName(self,filename):
+    def check_fileformat(self,filename):
+        isSuccess = True
         try:
             if '.html' in filename or '.txt' in filename:
                 if os.stat(filename).st_size != 0:
-                    current_file = open(filename)
-                    self.file_name = filename
-                    fileRead = current_file.read()
-                    self.reading = fileRead
+                   #print "filename correct format"
+                    isSuccess = True
                 else:
                     print '%s is empty file' %filename
+                    isSuccess = False
             else:
                 print'%s does not exist in current directory' %filename
+                isSuccess = False
         except IndexError:
+            isSuccess = False
             print 'You failed to provide filename as input on the command line!'
-        return filename
+        return filename,isSuccess
 
-    def check_Attributes(self):
+    def open_file(self,filename,isSuccess):
         try:
-            remove_params = re.sub(r'<!.+-->',r' ',self.reading)
-            self.texts = remove_params
-            
-            for tags in re.findall(r'<([^/][^>]*)>',self.texts):
-                if ' ' in tags:
-                    for attribute in re.findall('([a-z]+)? *([a-z-]+)="([^"]+)',tags):
-                        HtmlTags.append(attribute[0])
-                        self.htmltags = HtmlTags
-                        HtmlAttributes.append(attribute[1])
-                        self.htmlAttributes = HtmlAttributes
-                        HtmlAttributeValues.append(attribute[2])
-                        self.htmlAttributeValues = HtmlAttributeValues
-                else:
-                    HtmlTags.append(tags)
-            HtmlTags =filter(None, HtmlTags)
+            if isSuccess == True:
+                current_file = open(filename)
+                fileRead = current_file.read()
+                self.reading = fileRead
+            else:
+                isSuccess = False
+        except Exception as exception:
+            isSuccess = False
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            message = template.format(type(exception).__name__, exception.args)
+            print message
+        return fileRead,isSuccess
 
-        except:
-            pass
-        return HtmlTags
+    def check_fileAttributes(self,filename,isSuccess):
+        if isSuccess == True: 
+            try: 
+                HtmlTags=[]
+                HtmlAttributes=[]
+                HtmlAttributeValues=[]
+                remove_params = re.sub(r'<!.+-->',r' ',filename)
+                self.texts = remove_params
+                
+                for tags in re.findall(r'<([^/][^>]*)>',self.texts):
+                    if ' ' in tags:
+                        for attribute in re.findall('([a-z]+)? *([a-z-]+)="([^"]+)',tags):
+                            HtmlTags.append(attribute[0])
+                            HtmlTags =filter(None, HtmlTags)
+                            self.htmltags = HtmlTags
+                            HtmlAttributes.append(attribute[1])
+                            self.htmlAttributes = HtmlAttributes
+                            HtmlAttributeValues.append(attribute[2])
+                            self.htmlAttributeValues = HtmlAttributeValues
+                    else:
+                        HtmlTags.append(tags)
 
-    def display(self):
-        isSuccess = True
-        try:
-            choice = int(raw_input("""
+            except Exception as exception:
+                isSuccess = False
+                template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+                message = template.format(type(exception).__name__, exception.args)
+                print message
+            return HtmlTags, isSuccess
+
+    def display(self,isSuccess):
+        if isSuccess == True:
+            try:
+                choice = int(raw_input("""
     1 for tags,
     2 for attributes,
     3 for attribute values,
     4 for search attribute value,
     5.exit
-    Enter number:"""))
-            if choice == 1:
-                for tagg in self.htmltags:
-                    print tagg
-                self.display()
+        Enter number:"""))
+                if choice == 1:
+                    for tag in self.htmltags:
+                        print tag
+                    self.display(isSuccess)
 
-            elif choice == 2:
-                for attrib in self.htmlAttributes:
-                    print attrib
-                self.display()
+                elif choice == 2:
+                    for attrib in self.htmlAttributes:
+                        print attrib
+                    self.display(isSuccess)
 
-            elif choice == 3:
-                for attrib,values in map(None, self.htmlAttributes,self.htmlAttributeValues):
-                    print "%s = %s"%(attrib,values)
-                self.display()
+                elif choice == 3:
+                    for attrib,values in map(None, self.htmlAttributes,self.htmlAttributeValues):
+                        print "%s = %s"%(attrib,values)
+                    self.display(isSuccess)
 
-            elif choice == 4:
-                attrib = str(raw_input('enter attribe ='))
-                if attrib in self.htmlAttributes:
-                    index_value = self.htmlAttributes.index(attrib)
-                    print "Attribute value:",self.htmlAttributeValues[index_value]
+                elif choice == 4:
+                    attrib = str(raw_input('enter attribe ='))
+                    if attrib in self.htmlAttributes:
+                        index_value = self.htmlAttributes.index(attrib)
+                        print "Attribute value:",self.htmlAttributeValues[index_value]
+                    else:
+                        print "Values doesn't match"
+                    self.display(isSuccess)
+
+                elif choice == 5:
+                    sys.exit()
                 else:
-                    print "Values doesn't match"
-                self.display()
-            elif choice == 5:
-                sys.exit()
-            else:
-                print "Enter correct number"
-                self.display()
-        except Exception as exception:
-            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-            message = template.format(type(exception).__name__, exception.args)
-            print message
-            isSuccess = False
-            pass
+                    print "Enter correct number"
+                    self.display(isSuccess)
+    
+            except Exception as exception:
+                isSuccess = False
+                template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+                message = template.format(type(exception).__name__, exception.args)
+                print message
         return isSuccess
         
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         html = CheckHTML()
-        html.get_FileName(sys.argv[1])
-        html.check_Attributes()
-        html.display()
-
+        arg1, arg2 = html.check_fileformat(sys.argv[1])
+        
+        if arg2:
+            arg3, arg4 = html.open_file(arg1,arg2)
+           
+        if arg4:
+            arg5,arg6 = html.check_fileAttributes(arg3,arg4)
+            
+        if arg6:
+            html.display(arg6)
--- Python_Exercises/test_html.py
+++ Python_Exercises/test_html.py
@@ -2,24 +2,38 @@ import unittest
 from Problem1 import CheckHTML
 
 class TestHtmlTags(unittest.TestCase):
-    def test_filename(self):
+
+    def setUp(self):
+        self.html_file = open("sample.txt")
+        self.read_file = self.html_file.read()
         
-        result=html.get_FileName('sample.txt')
+    def test_fileName(self):
+        result = html.check_fileformat('sample.txt')[0]
         self.assertEqual(result, 'sample.txt')
         self.assertNotEqual(result, 'sample')
 
-    def test_tags(self):
-        result = html.check_Attributes()
-        expectedresult=(['head', 'title', 'object', 'param'])
-        self.assertEqual(result, expectedresult)
-        self.assertNotEqual(result,"['head','object', 'param']")
 
-    def test_choice(self):
-        result = html.display()
-        print result
-        self.assertEqual(result,False)
-        
+    def test_fileReturnTags(self):
+        test_open = html.open_file('sample.txt',True)[0]
+        self.assertIn(test_open, self.read_file) 
+
+    def test_fileAttributes(self):
+        return_value = html.check_fileAttributes(self.read_file,True)[0]
+        self.assertEqual(return_value, ['head', 'title', 'object', 'param'])
+   
 if __name__ == '__main__':
-    
     html = CheckHTML()
     unittest.main()
+
+
+
+
+
+
+
+
+
+
+
+
+
