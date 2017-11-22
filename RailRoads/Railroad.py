36460973866ec25f8f84ecb66243891fe7f41a9d
omprakashnd <omprakashnd94@gmail.com>
2017-11-22 12:46:35 +0530

    RailRoad Problem2

--- RailRoads/Railroad.py
+++ RailRoads/Railroad.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 class RailRoad():
     def GetCities(self):
         cities = []
-        ncity = int(raw_input("Enter no.of cities "))
+        ncity = int(raw_input("Enter number of cities "))
         for i in range(ncity):
             i=i+1
             city = str(raw_input("Enter %r cities name:" %i))
@@ -12,80 +12,75 @@ class RailRoad():
         return cities 
 
     def TrainDetails(self,cities):
-        traindetp = []
-        trainarr =[]
-        notrain = int(raw_input("Enter no.of trains"))
-        for train in range(notrain):
-            
-            start_info = str(raw_input("enter starttime and depature station:"))
+        traindepaturelist = []
+        trainarrivallist =[]
+        notrain = int(raw_input("Enter number of trains to connected city's"))
+
+        for train in range(notrain):    
+
+            start_info = str(raw_input("enter train depature time and depature station:"))
             start = start_info.split()
-            changed_list = [int(f) if f.isdigit() else f for f in start]
-            traindetp.append(changed_list)
+            
+            start_list = [int(check) if check.isdigit() else check for check in start]
+            traindepaturelist.append(start_list)
 
-            end_info = str(raw_input("enter reachtime and arrival station:"))
+            end_info = str(raw_input("enter train arrival time and arrival station:"))
             end = end_info.split()
-            changed_list1 = [int(f) if f.isdigit() else f for f in end]
-            trainarr.append(changed_list1)
 
-        return traindetp,trainarr 
+            end_list = [int(check) if check.isdigit() else check for check in end]
+            trainarrivallist.append(end_list)
+        return traindepaturelist,trainarrivallist 
 
     def PassengerInfo(self,city):
-
         passenger =[]
-
         passenger_start = str(raw_input("enter passener startpoint:"))
         if passenger_start in city:
             passenger.append(passenger_start)
-            
             passenger_end = str(raw_input("enter passener endpoint:"))
             if passenger_end in city:
                 passenger.append(passenger_end)
                 passenger_time = int(raw_input("enter time to reach destination:"))
                 if passenger_time <=24:
                     passenger.append(passenger_time)
-                    print "Process.."
                 else:
                     print "Enter correctime.."
             else:
                  print "Wrong End Point"     
         else:
             print "Wrong Start Point"
-
         return passenger
 
-    def check(self,traindept,trainarr,passenger):
-        dest = []
-        start = []
-        
-        for i in range(0,len(trainarr)):
+    def MatchStation(self,traindepaturelist,trainarrivallist,passenger):
+        destination = []
+        for i in range(0,len(trainarrivallist)):
             j=1
-            if passenger[j] == trainarr[i][j]:
-                 dest.append(trainarr[i])
+            if passenger[j] == trainarrivallist[i][j]:
+                 destination.append(trainarrivallist[i])
             else:
                 print "Wrong Destination"
-        return dest
+        return destination
 
-    def destination(self,dest,passenger):
+    def MatchTime(self,destination,passenger):
         getDest=[]
-        res =""
+        check =""
         j=0
-        for i in range(0,len(dest)):
-            if passenger[2] == dest[i][j]:
-                getDest.append(dest[i])        
-            elif passenger[2] > dest[i][j]:
-                getDest.append(dest[i])
+        for i in range(0,len(destination)):
+            if passenger[2] == destination[i][j]:
+                getDest.append(destination[i])        
+            elif passenger[2] > destination[i][j]:
+                getDest.append(destination[i])
             else:
-                res = "No connection"
+                check = "No connection"
         Dest=max(getDest)
         if len(getDest) <= 1:
-            print res
+            print check
         return Dest
 
-    def checkingDest(self,getDest,trainarr,traindept):
-        for i in range(len(trainarr)):
-            if trainarr[i] == getDest:
-                print "If You start %r clock from %r" %(traindept[i][0], traindept[i][1])
-                print "You will reach %r on %r clock"% (trainarr[i][1], trainarr[i][0])
+    def Result(self,dest,trainarrivallist,traindepaturelist):
+        for i in range(len(trainarrivallist)):
+            if trainarrivallist[i] == dest:
+                print "If You start %r clock from %r" %(traindepaturelist[i][0], traindepaturelist[i][1])
+                print "You will reach %r on %r clock"% (trainarrivallist[i][1], trainarrivallist[i][0])
 
 if __name__ == '__main__':
 
@@ -97,11 +92,11 @@ if __name__ == '__main__':
     
     arg3 = Rail.PassengerInfo(arg)
 
-    arg4 = Rail.check(arg1,arg2,arg3)
+    arg4 = Rail.MatchStation(arg1,arg2,arg3)
 
-    arg5=Rail.destination(arg4,arg3)
+    arg5=Rail.MatchTime(arg4,arg3)
     
-    Rail.checkingDest(arg5,arg2,arg1)
+    Rail.Result(arg5,arg2,arg1)
 
 
     
