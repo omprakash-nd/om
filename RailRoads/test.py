0e8538dd0f3dde2505c3b6d505968204ed755b1a
omprakashnd <omprakashnd94@gmail.com>
2017-12-05 11:17:17 +0530

    Unittest-RailRoad

--- RailRoads/test.py
+++ RailRoads/test.py
@@ -6,23 +6,31 @@ class TestRailRoads(unittest.TestCase):
     def setUp(self):
         self.csv_file = open("testinput.csv")
         self.read_file = csv.reader(self.csv_file)
+        departure,arrival,cities,result=Roads.readRailInfo("testinput.csv")
+        self.city=cities
+        self.start_station=departure
+        self.end_station=arrival
 
     def test_Fileinfo(self):
         departure,arrival,cities,result=Roads.readRailInfo("testinput.csv")
         self.assertTrue(result)
 
-    def test_WrongFileName(self):
-        departure,arrival,cities,result=Roads.readRailInfo("testinp.csv")
-        print result
+    def test_cities(self):
+        test_city=['madurai', 'chennai', 'tambaram', 'vilupuram', 'kovai', 'dindugal', 'tiruchy']
+        departure,arrival,cities,result=Roads.readRailInfo("testinput.csv")
+        self.assertEqual =(cities,test_city) 
 
-        
-##    def test_readFile(self):
-##        filename =self.csv_file
-##        Result = Roads.readFile(filename,True)[3]
-##        for data in Result:
-##            if data == "['chennai', 'madurai']":
-##      self.assertIn(Result,data)
-##      
+    def test_NotListCities(self):
+        test_city=['tambaram', 'vilupuram', 'kovai', 'dindugal']
+        departure,arrival,cities,result=Roads.readRailInfo("testinput.csv")
+        self.city=cities
+        self.assertNotEqual =(cities,test_city)
+
+    def test_PassengerInfo(self):
+        list_city=self.city
+        Result,SuccessResult=Roads.getPassengerInfo(list_city,True)
+        self.assertTrue(SuccessResult)
+      
 if __name__ == '__main__':
 
     Roads = Railroads()
