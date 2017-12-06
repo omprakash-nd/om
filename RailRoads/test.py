9c796b7b993265419c7410de0196e969000d89da
omprakashnd <omprakashnd94@gmail.com>
2017-12-06 19:22:54 +0530

    Unit test

--- RailRoads/test.py
+++ RailRoads/test.py
@@ -1,38 +1,46 @@
-import unittest,csv
+import unittest
+import csv
 from Railroad import *
 
 class TestRailRoads(unittest.TestCase):
-
     def setUp(self):
         self.csv_file = open("testinput.csv")
         self.read_file = csv.reader(self.csv_file)
-        departure,arrival,cities,result=Roads.readRailInfo("testinput.csv")
-        self.city=cities
-        self.start_station=departure
-        self.end_station=arrival
+        departure, arrival, cities, result = Roads.readRailInfo("testinput.csv")
+        self.city = cities
+        self.start_station = departure
+        self.end_station = arrival
 
-    def test_Fileinfo(self):
-        departure,arrival,cities,result=Roads.readRailInfo("testinput.csv")
+    def test_file(self):
+        result = Roads.readRailInfo("testinput.csv")
         self.assertTrue(result)
 
-    def test_cities(self):
-        test_city=['madurai', 'chennai', 'tambaram', 'vilupuram', 'kovai', 'dindugal', 'tiruchy']
-        departure,arrival,cities,result=Roads.readRailInfo("testinput.csv")
-        self.assertEqual =(cities,test_city) 
+    def test_file(self):
+        result = Roads.readRailInfo("testinput.txt")
+        self.assertTrue(result)
 
-    def test_NotListCities(self):
-        test_city=['tambaram', 'vilupuram', 'kovai', 'dindugal']
-        departure,arrival,cities,result=Roads.readRailInfo("testinput.csv")
-        self.city=cities
-        self.assertNotEqual =(cities,test_city)
+    def test_passenger(self):
+        list_city = self.city
+        passenger=['tambaram', 'vilupuram',1200]
+        start = passenger[0]
+        end = passenger[1]
+        Result, SuccessResult = Roads.checkPassenger(list_city, passenger, True)
+        self.assertIn(end, list_city)
 
-    def test_PassengerInfo(self):
-        list_city=self.city
-        Result,SuccessResult=Roads.getPassengerInfo(list_city,True)
-        self.assertTrue(SuccessResult)
-      
+    def test_wrong_stations(self):
+        list_city = self.city
+        passenger = ['tambaram', '', 1200]
+        end = passenger[1]
+        Roads.checkPassenger(list_city, passenger, True)
+        self.assertNotEqual(end, list_city)
+
+    def test_wrong_time(self):
+        list_city = self.city
+        passenger=['tambaram', 'chennai', 26345]
+        time = passenger[2]
+        Roads.checkPassenger(list_city, passenger, True)
+        self.assertNotEqual(time, list_city)
+    
 if __name__ == '__main__':
-
     Roads = Railroads()
     unittest.main()
-
