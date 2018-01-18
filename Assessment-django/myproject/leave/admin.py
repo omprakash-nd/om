a20da39ce019fa32c6d3699170531fed549ca959
omprakashnd <omprakashnd94@gmail.com>
2018-01-18 11:52:25 +0530

    Assessment

--- Assessment-django/myproject/leave/admin.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.contrib import admin
-from models import Employee, LeaveForm
-# Register your models here.
-admin.site.register(Employee)
-admin.site.register(LeaveForm)
--- Assessment-django/myproject/leave/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from models import LeaveForm
-
-
-class UserLeave(serializers.ModelSerializer):
-	class Meta:
-		model = LeaveForm
-		fields = '__all__'
